f3030f2ab6480dd665d3333fa5fa078358652e1f
GABBRIELJUA <20221040@uthh.edu.mx>
2025-07-22 10:57:05 -0600

    recomendaciones

--- app.py
+++ app.py
@@ -1,25 +1,67 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # 👈 IMPORTANTE
+from flask_cors import CORS 
 import joblib
 import logging
 import pandas as pd
 
 app = Flask(__name__)
-CORS(app)  # Permite CORS desde cualquier origen
+CORS(app)
 
 logging.basicConfig(level=logging.INFO)
 
-# Cargar modelo de reglas de asociación
 try:
     rules: pd.DataFrame = joblib.load('reglas_asociacion.pkl')
     logging.info("Modelo de reglas de asociación cargado exitosamente.")
 except Exception as e:
     logging.error(f"Error al cargar el modelo: {e}")
-    rules = pd.DataFrame()  # Evita errores si carga falla
+    rules = pd.DataFrame()
 
 @app.route('/')
 def home():
-    return "API corriendo. Usa POST a /recomendar con JSON"
+    return """
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>API de Recomendación</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f5f5f5;
+                color: #333;
+                display: flex;
+                justify-content:start;
+                align-items: start;
+                height: 100vh;
+                margintop: 50px;
+                margin-left: 50px;
+            }
+            .box {
+                background: #fff;
+                padding: 20px 30px;
+                border: 1px solid #ddd;
+                border-radius: 8px;
+                box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+                text-align: center;
+            }
+            code {
+                background-color: #eee;
+                padding: 2px 6px;
+                border-radius: 4px;
+                font-family: monospace;
+                color: #c0392b;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="box">
+            <h2>API de Recomendación</h2>
+            <p>Está corriendo correctamente</p>
+            <p>Haz POST a <code>/recomendar</code>
+        </div>
+    </body>
+    </html>
+    """
 
 @app.route('/recomendar', methods=['POST'])
 def recomendar():
@@ -37,8 +79,8 @@ def recomendar():
             if set(row['antecedents']).issubset(productos_set):
                 recomendaciones.update(set(row['consequents']))
 
-        recomendaciones -= productos_set  # Quitar productos ya seleccionados
-        recomendaciones = set(r.strip() for r in recomendaciones)  # Limpiar espacios
+        recomendaciones -= productos_set 
+        recomendaciones = set(r.strip() for r in recomendaciones) 
 
         return jsonify(sorted(recomendaciones))
 
